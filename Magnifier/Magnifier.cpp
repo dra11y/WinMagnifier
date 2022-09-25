738ed67b855a019acc9ed6908270ac8eb444149d
Ahmed Tarek <pushqrdx@gmail.com>
2022-09-25 11:30:53 +0200

    rewrite for better performance, not causing cursor lag anymore

--- Magnifier/Magnifier.cpp
+++ Magnifier/Magnifier.cpp
@@ -1,181 +1,163 @@
-#include <dwmapi.h>
-#include <magnification.h>
-
-#include <iostream>
-#include <thread>
+/*
+ * This file is part of the Magnifier distribution (https://github.com/pushqrdx/Magnifier).
+ * Copyright (c) 2022 Ahmed Tarek.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "Magnifier.h"
 
-#pragma comment(lib, "dwmapi.lib")
-#pragma comment(lib, "magnification.lib")
-
-HHOOK MouseHook;
-HHOOK KeyboardHook;
-MSG CurrentMessage;
-
-Spring MouseZTweener;
-Spring MouseXTweener;
-Spring MouseYTweener;
-
-int MouseX;
-int MouseY;
-bool ModDown = false;
-int Modifiers = Modifiers::NONE;
-float State = 1.0f;
-bool Running = true;
-
-LRESULT CALLBACK MouseProc(int code, WPARAM wParam, LPARAM lParam)
+auto Clamp(auto value, auto min, auto max)
 {
-	if (ModDown || MouseZTweener > 1.0f)
+	return max(min, min(value, max));
+}
+
+auto GetCursorPosition()
+{
+	POINT pnt;
+	GetCursorPos(&pnt);
+	return pnt;
+}
+
+HHOOK MouseHook = nullptr;
+HHOOK KeyboardHook = nullptr;
+
+int Modifiers = Modifiers::NONE;
+int CurrentKey = 0x0;
+
+int MouseZ = 1;
+
+auto CALLBACK MouseHookProc(int code, WPARAM wParam, LPARAM lParam)
+{
+	if (auto data = (MSLLHOOKSTRUCT*)lParam)
 	{
-		if (auto mData = cast(MSLLHOOKSTRUCT*)lParam)
+		if (wParam == WM_MOUSEWHEEL)
 		{
-			MouseX = mData->pt.x;
-			MouseY = mData->pt.y;
-
-			MouseXTweener = cast(float)MouseX;
-			MouseYTweener = cast(float)MouseY;
-
-			if (ModDown && wParam == WM_MOUSEWHEEL)
-			{
-				if (HIWORD(mData->mouseData) == 120)
-				{
-					MouseZTweener += 0.25f;
-				}
-				else
-				{
-					MouseZTweener = max(1.0f, MouseZTweener - 0.25f);
-				}
-
-				return 1;
-			}
+			MouseZ = Clamp(MouseZ + (HIWORD(data->mouseData) == 120 ? 1 : -1), 1, 20);
+			return (LRESULT)0x1;
 		}
 	}
 
 	return CallNextHookEx(MouseHook, code, wParam, lParam);
 }
 
-LRESULT CALLBACK KeyboardProc(int code, WPARAM wParam, LPARAM lParam)
+auto CALLBACK KeyboardHookProc(int code, WPARAM wParam, LPARAM lParam)
 {
-	// TODO(pushqrdx) Refactor this shit out, also keyup/down handling is so messy
-	if (auto kData = cast(KBDLLHOOKSTRUCT*)lParam)
+	if (auto data = (KBDLLHOOKSTRUCT*)lParam)
 	{
-		auto key = kData->vkCode;
+		CurrentKey = data->vkCode;
 
-		if (wParam == WM_KEYUP && (key == VK_LWIN || key == VK_LSHIFT))
+		if (wParam == WM_KEYUP && (CurrentKey == VK_LWIN || CurrentKey == VK_LSHIFT))
 		{
 			Modifiers = 0x0;
-			ModDown = false;
+			CurrentKey = 0x0;
 			return CallNextHookEx(KeyboardHook, code, wParam, lParam);
 		}
 
-		switch (key)
+		switch (CurrentKey)
 		{
 		case VK_LWIN:
 			Modifiers |= Modifiers::LWIN;
 			break;
-		case VK_LMENU:
-			Modifiers |= Modifiers::LALT;
-			break;
-		case VK_LCONTROL:
-			Modifiers |= Modifiers::LCONTROL;
-			break;
+			//case VK_LMENU:
+			//	Modifiers |= Modifiers::LALT;
+			//	break;
+			//case VK_LCONTROL:
+			//	Modifiers |= Modifiers::LCONTROL;
+			//	break;
 		case VK_LSHIFT:
 			Modifiers |= Modifiers::LSHIFT;
 			break;
-		}
-
-		ModDown = (Modifiers ^ (Modifiers::LWIN | Modifiers::LSHIFT)) == 0;
-
-		if (ModDown)
-		{
-			switch (key)
-			{
-			case VK_Q:
-				Running = false;
-				return 1;
-			case VK_E:
-			{
-				if (wParam == WM_KEYUP)
-					if (MouseZTweener > 1.0f)
-					{
-						State = MouseZTweener;
-						MouseZTweener = 1.0f;
-					}
-					else
-					{
-						POINT pt;
-						if (GetCursorPos(&pt))
-						{
-							MouseX = pt.x;
-							MouseY = pt.y;
-							MouseXTweener.reset(cast(float)MouseX);
-							MouseYTweener.reset(cast(float)MouseY);
-						}
-
-						MouseZTweener = State;
-					}
-				return 1;
-			}
-			}
 		}
 	}
 
 	return CallNextHookEx(KeyboardHook, code, wParam, lParam);
 }
 
-int main()
+auto main() -> int
 {
-	FreeConsole();
-
-	DWORD MainThreadID = GetCurrentThreadId();
-	std::thread MagnifierThread([&]()
-		{
-			if (!MagInitialize())
-			{
-				puts("Cannot initialize magnifier");
-				return 1;
-			}
-
-			auto sw = GetSystemMetrics(SM_CXSCREEN);
-			auto sh = GetSystemMetrics(SM_CYSCREEN);
-			auto clamp = [](auto value, auto min, auto max)
-			{
-				return max(min, min(value, max));
-			};
-
-			while (Running)
-			{
-				auto x = cast(float)MouseXTweener;
-				auto y = cast(float)MouseYTweener;
-				auto z = cast(float)MouseZTweener;
-				auto f = (1.0 - (1.0 / z));
-				auto zw = cast(int)(f * sw);
-				auto zh = cast(int)(f * sh);
-				auto zx = cast(int)(f * x);
-				auto zy = cast(int)(f * y);
-
-				MagSetFullscreenTransform(z, clamp(zx, 0, zw), clamp(zy, 0, zh));
-
-				DwmFlush();
-			}
-
-			MagUninitialize();
-			PostThreadMessage(MainThreadID, WM_QUIT, 0, 0);
-			return 0;
-		});
-	MouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseProc, NULL, NULL);
-	KeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, NULL);
-	MagnifierThread.detach();
-
-	while (GetMessage(&CurrentMessage, NULL, 0, 0))
+	// FreeConsole();
+	if (!MagInitialize())
 	{
+		puts("Cannot initialize magnifier");
+		return 1;
+	}
+
+	auto MainThreadID = GetCurrentThreadId();
+	auto ScreenWidth = GetSystemMetrics(SM_CXSCREEN);
+	auto ScreenHeight = GetSystemMetrics(SM_CYSCREEN);
+	auto SmoothMouseX = Spring();
+	auto SmoothMouseY = Spring();
+	auto SmoothMouseZ = Spring();
+	auto CurrentMessage = MSG();
+	auto VSync = std::thread([&] {
+		while (true)
+		{
+			PostThreadMessage(MainThreadID, 0x0420, 0, 0);
+			DwmFlush();
+		}
+
+		return 0;
+		});
+	VSync.detach();
+
+	KeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardHookProc, NULL, NULL);
+
+	while (GetMessage(&CurrentMessage, 0, 0, 0))
+	{
+		if ((Modifiers ^ (Modifiers::LWIN | Modifiers::LSHIFT)) == 0)
+		{
+			switch (CurrentKey)
+			{
+			case VK_Q: {
+				PostThreadMessage(MainThreadID, WM_QUIT, 0, 0);
+				break;
+			}
+			}
+
+			if (not MouseHook)
+			{
+				MouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseHookProc, NULL, NULL);
+			}
+		}
+		else
+		{
+			if (MouseHook)
+			{
+				UnhookWindowsHookEx(MouseHook);
+				MouseHook = nullptr;
+			}
+		}
+
+		// TODO(pushqrdx): Avoid doing this work on each loop by detecting idle state.
+		auto [MouseX, MouseY] = GetCursorPosition();
+		auto z = SmoothMouseZ(MouseZ);
+		auto f = (1.0 - (1.0 / z));
+		auto pw = (f * ScreenWidth);
+		auto ph = (f * ScreenHeight);
+		auto w = (ScreenWidth - pw);
+		auto h = (ScreenHeight - ph);
+
+		MagSetFullscreenTransform(z, Clamp(SmoothMouseX(MouseX) - (w / 2), 0, pw), Clamp(SmoothMouseY(MouseY) - (h / 2), 0, ph));
+
 		TranslateMessage(&CurrentMessage);
 		DispatchMessage(&CurrentMessage);
 	}
 
-	UnhookWindowsHookEx(MouseHook);
 	UnhookWindowsHookEx(KeyboardHook);
+	if (MouseHook)
+		UnhookWindowsHookEx(MouseHook);
+	MagUninitialize();
+
 	return 0;
 }
-
--- Magnifier/Magnifier.h
+++ Magnifier/Magnifier.h
@@ -1,48 +1,42 @@
+/*
+ * This file is part of the Magnifier distribution (https://github.com/pushqrdx/Magnifier).
+ * Copyright (c) 2022 Ahmed Tarek.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
-#define cast(...) (__VA_ARGS__)
+#include <thread>
+
+#include <dwmapi.h>
+#include <magnification.h>
+
+#pragma comment(lib, "dwmapi.lib")
+#pragma comment(lib, "magnification.lib")
+
 #define VK_E 0x45
 #define VK_Q 0x51
 #define VK_T 0x54
 
 struct Spring
 {
-	operator float()
+	auto operator()(float target)
 	{
+		this->target = target;
 		velocity += (target - current) * acceleration;
 		velocity *= friction;
 		return current += velocity;
-	}
-
-	auto reset(float destination)
-	{
-		current = target = destination;
-		velocity = 0.0;
-	}
-
-	auto operator=(float destination)
-	{
-		target = destination;
-	}
-
-	auto operator-(float other)
-	{
-		return target - other;
-	}
-
-	auto operator+=(float other)
-	{
-		target += other;
-	}
-
-	auto operator-=(float other)
-	{
-		target -= other;
-	}
-
-	auto operator<=>(float other)
-	{
-		return target <=> other;
 	}
 
 private:
