bfafdba6493d4f29d1072b7df27bd6a3dc9653ab
Tom Grushka <tom@dra11y.com>
2024-08-08 17:17:21 -0600

    Keyboard hooks; prevent LWin from opening start menu

--- Magnifier/Magnifier.cpp
+++ Magnifier/Magnifier.cpp
@@ -1,25 +1,79 @@
-/*
- * This file is part of the Magnifier distribution (https://github.com/pushqrdx/Magnifier).
- * Copyright (c) 2022 Ahmed Tarek.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "Magnifier.h"
+
+HHOOK MouseHook = nullptr;
+HHOOK KeyboardHook = nullptr;
+
+double MouseZ = 1;
+
+// Global variables to track LWin state
+std::atomic<bool> isLWinPressed{ false };
+std::atomic<bool> otherKeyPressedWithLWin{ false };
+std::chrono::time_point<std::chrono::steady_clock> lWinPressTime;
 
 static auto Clamp(auto value, auto min, auto max)
 {
 	return max(min, min(value, max));
+}
+
+LRESULT CALLBACK KeyboardHookProc(int code, WPARAM wParam, LPARAM lParam)
+{
+	if (code >= 0)
+	{
+		auto key = ((KBDLLHOOKSTRUCT*)lParam)->vkCode;
+		if (wParam == WM_KEYDOWN)
+		{
+			if (key == VK_LWIN)
+			{
+				isLWinPressed = true;
+				otherKeyPressedWithLWin = false;
+				lWinPressTime = std::chrono::steady_clock::now();
+				// return 1; // Trap the LWin key press
+			}
+			else if (isLWinPressed)
+			{
+				otherKeyPressedWithLWin = true;
+			}
+
+			if ((GetAsyncKeyState(VK_LWIN) & 0x8000) && isLWinPressed) // Check if Left Win key is pressed
+			{
+				if (key == VK_OEM_PLUS)
+				{
+					MouseZ *= 1.25;
+					MouseZ = Clamp(MouseZ, 1.0, 20.0);
+					return 1; // Trap the key
+				}
+				else if (key == VK_OEM_MINUS)
+				{
+					MouseZ /= 1.25;
+					MouseZ = Clamp(MouseZ, 1.0, 20.0);
+				}
+			}
+		}
+		else if (wParam == WM_KEYUP)
+		{
+			if (key == VK_LWIN)
+			{
+				if (isLWinPressed && !otherKeyPressedWithLWin)
+				{
+					auto duration = std::chrono::steady_clock::now() - lWinPressTime;
+					if (duration < std::chrono::milliseconds(500))
+					{
+						// If LWin was pressed and released quickly without any other key press, trap it
+						isLWinPressed = false;
+
+						// Simulate left control key press
+						keybd_event(VK_LCONTROL, 0, 0, 0);
+
+						// Simulate left control key release
+						keybd_event(VK_LCONTROL, 0, KEYEVENTF_KEYUP, 0);
+					}
+				}
+				isLWinPressed = false;
+			}
+		}
+	}
+
+	return CallNextHookEx(KeyboardHook, code, wParam, lParam);
 }
 
 static auto GetCursorPosition()
@@ -28,9 +82,6 @@ static auto GetCursorPosition()
 	GetCursorPos(&pnt);
 	return pnt;
 }
-
-HHOOK MouseHook = nullptr;
-double MouseZ = 1;
 
 static auto CALLBACK MouseHookProc(int code, WPARAM wParam, LPARAM lParam)
 {
@@ -52,10 +103,22 @@ static auto CALLBACK MouseHookProc(int code, WPARAM wParam, LPARAM lParam)
 	return CallNextHookEx(MouseHook, code, wParam, lParam);
 }
 
-auto main() -> int
+
+
+int APIENTRY WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nShowCmd
+)
+//int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-#ifdef NDEBUG
-	FreeConsole();
+#ifdef _DEBUG
+	AllocConsole();
+	FILE* fDummy;
+	freopen_s(&fDummy, "CONOUT$", "w", stdout);
+	freopen_s(&fDummy, "CONOUT$", "w", stderr);
+	freopen_s(&fDummy, "CONIN$", "r", stdin);
 #endif
 	if (not MagInitialize() or not SetProcessDPIAware())
 	{
@@ -77,6 +140,8 @@ auto main() -> int
 
 		return 0;
 	}).detach();
+
+	KeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardHookProc, NULL, NULL);
 
 	while (GetMessage(&CurrentMessage, 0, 0, 0))
 	{
@@ -128,6 +193,10 @@ auto main() -> int
 
 	if (MouseHook)
 		UnhookWindowsHookEx(MouseHook);
+
+	if (KeyboardHook)
+		UnhookWindowsHookEx(KeyboardHook);
+
 	MagUninitialize();
 
 	return 0;
--- Magnifier/Magnifier.h
+++ Magnifier/Magnifier.h
@@ -1,26 +1,9 @@
-/*
- * This file is part of the Magnifier distribution (https://github.com/pushqrdx/Magnifier).
- * Copyright (c) 2022 Ahmed Tarek.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
 #pragma once
 
-#include <thread>
-
+#include <atomic>
 #include <dwmapi.h>
 #include <magnification.h>
+#include <thread>
 
 #pragma comment(lib, "dwmapi.lib")
 #pragma comment(lib, "magnification.lib")
@@ -48,7 +31,7 @@ private:
 	float target = 1.0;
 	float current = 1.0;
 	float velocity = 0.0;
-	float acceleration = 0.4f;
+	float acceleration = 0.9f;
 	float friction = 0.3f;
 };
 
